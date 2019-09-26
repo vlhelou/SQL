d16f6e3fbf141c4c9d33956c6962296391faa66b
vlhelou <vlhelou1971@gmail.com>
2019-09-26 14:30:33 -0300

    Melhoria

--- Bases/UsuarioOrfão.sql
+++ Bases/UsuarioOrfão.sql
@@ -1,6 +1,10 @@
-select db.*
+select 
+    db.name
+    , db.create_date
+    , db.modify_date 
+    , case when EXISTS(select * from sys.server_principals pr where pr.name collate Latin1_General_CI_AI = db.name collate Latin1_General_CI_AI)  then 'Tem no Servidor' else 'Somente na base' end
 from sys.database_principals db
-    left join sys.server_principals principal on db.sid = principal.sid
+    left outer join sys.server_principals principal on db.sid = principal.sid
 WHERE
     principal.sid is null
     and authentication_type_desc='INSTANCE'
