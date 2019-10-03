eb1acaa1410c008948ff8ddd5dd14c158b2c88fb
vlhelou <vlhelou1971@gmail.com>
2019-10-03 11:04:30 -0300

    Update lista tabela com qt colunas e linhas.sql

--- lista tabela com qt colunas e linhas.sql
+++ lista tabela com qt colunas e linhas.sql
@@ -1,13 +1,10 @@
 select 
 	tables.name
-	, COUNT(1) colunas
+	, COUNT( distinct syscolumns.colid) colunas
 	, avg(partitions.rows) Linhas
 from sys.tables
 	inner join sys.syscolumns on tables.object_id = syscolumns.id
 	inner join sys.partitions on tables.object_id = partitions.object_id
-
 group by 
 	tables.name
-
-order by Linhas desc
-
+order by colunas desc
