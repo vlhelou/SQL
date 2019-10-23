43b7275c6c1854fd653019326c5550940a75a7a9
vlhelou <vlhelou1971@gmail.com>
2019-10-23 13:35:44 -0300

    evolucao de consulta

--- Tabelas/Descobre tabela com 2 colunas especificas.sql
+++ Tabelas/Descobre tabela com 2 colunas especificas.sql
@@ -1,10 +1,23 @@
+declare @tcolunas table (NomeColuna varchar(100))
+insert into @tcolunas (NomeColuna) values ('CODSEGMENTO'),('CODTURMA');
+
+with qbase as (
+	select 
+		TABLE_SCHEMA
+		, TABLE_NAME
+		, COLUMN_NAME
+	from INFORMATION_SCHEMA.COLUMNS
+	where 
+		COLUMN_NAME in (select NomeColuna from @tcolunas)
+)
+
 select 
 	TABLE_SCHEMA
 	, TABLE_NAME
-	, COUNT(case when COLUMN_NAME in ('CODSEGMENTO','CODALUNO') then 1 else null end ) as qtColunas
-from INFORMATION_SCHEMA.COLUMNS
-group by 
+	, count(1) QtColunas
+from qbase
+group by
 	TABLE_SCHEMA
 	, TABLE_NAME
-having 
-	COUNT(case when COLUMN_NAME in ('CODSEGMENTO','CODALUNO') then 1 else null end )=2
+having COUNT(1) = (select count(1) from @tcolunas)
+order by TABLE_NAME
