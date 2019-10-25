c2859878c45d6590cccd4d6c48449e1534f168f7
vlhelou <vlhelou1971@gmail.com>
2019-10-25 00:44:13 -0300

    ajuste de tipos

--- Tabelas/Colunas para Propriedades do C#.sql
+++ Tabelas/Colunas para Propriedades do C#.sql
@@ -1,4 +1,3 @@
-
 select 
 	cl.name
 	, cl.isnullable
@@ -12,10 +11,13 @@ select
 			when 'char' then 'string'
 			when 'nvarchar' then 'string' 
 			when 'nchar' then 'string'
+			when 'xml' then 'string'
 			when 'datetime' then 'DateTime'
 			when 'decimal' then 'decimal'
 			when 'int' then 'int'
+			when 'bigint' then 'long'
 			when 'bit' then 'bool'
+			when 'uniqueidentifier' then 'Guid'
 			end +
 		case when cl.isnullable=1 and tp.name not in ('varchar','char','nvarchar','nchar') then '?' else '' end
 		+ ' '+cl.name+ ' { get; set; }' as c#
@@ -23,15 +25,15 @@ select
 		case when cl.isnullable=1 and tp.name not in ('varchar','char','nvarchar','nchar') then '?' else '' end
 		+': '+
 		case 
-		when tp.name in ('numeric', 'decimal','int') then 'number;'
+		when tp.name in ('numeric', 'decimal','int', 'bigint') then 'number;'
 		when tp.name in ('bit') then 'boolean;'
-		when tp.name in ('char', 'nchar','varchar','nvarchar') then 'string;'
+		when tp.name in ('char', 'nchar','varchar','nvarchar','uniqueidentifier','xml') then 'string;'
 		when tp.name in ('datetime', 'date','time') then 'Date;'
 		end as AngularInterface
 	, cl.name+': new FormControl(),' as AngularFormControl
 from sys.syscolumns cl
 	inner join sys.systypes tp on 
 		cl.xtype = tp.xtype
-where cl.id=OBJECT_ID('usuarios')
+where cl.id=OBJECT_ID('LMS_LogTreinamento_OnLine')
 	--and tp.name='datetime'
 order by cl.colid
