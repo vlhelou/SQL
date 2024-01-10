962dccdb99fdd19a72547e70ee0908672e7742ea
vlhelou <vlhelou1971@gmail.com>
2024-01-10 15:49:17 -0300

    colocar o nome do schema

--- lista tabela com qt colunas e linhas.sql
+++ lista tabela com qt colunas e linhas.sql
@@ -1,10 +1,11 @@
 select 
-	tables.name
+	schemas.name+'.'+tables.name Nome
 	, COUNT( distinct syscolumns.colid) colunas
 	, avg(partitions.rows) Linhas
 from sys.tables
+   inner join sys.schemas on tables.schema_id = schemas.schema_id
 	inner join sys.syscolumns on tables.object_id = syscolumns.id
 	inner join sys.partitions on tables.object_id = partitions.object_id
 group by 
-	tables.name
-order by colunas desc
+	schemas.name+'.'+tables.name
+order by linhas desc
